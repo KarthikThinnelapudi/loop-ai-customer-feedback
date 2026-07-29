f100671c65ee38b7cbca8d3f9ab747b2b0ff1ff5
Karthik Reddy <thinnelapudi123@gmail.com>
2026-07-29 18:43:43 +0530

    fix: render SessionTimeoutModal in DashboardLayout for 0 eslint warnings

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -37,8 +37,12 @@ export default function DashboardLayout({
 
   return (
     <div className="min-h-screen bg-slate-950 text-white flex relative overflow-hidden">
+      {/* Session Inactivity Warning Modal */}
+      <SessionTimeoutModal />
+
       {/* Sidebar */}
       <Sidebar />
+
 
       {/* Main Content Area */}
       <div className="flex-1 flex flex-col min-w-0">
